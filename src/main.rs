fc4044a7220966e29f667cb2d854d9e63457fa1d
Thibaut Artis <thibaut.artis@varnish-software.com>
2025-03-25 17:51:33 +0100

    First POC
    - Load PDF
    - Convert PDF to JPG
    - Feed JPG to AI model
    - Ask AI model to extract informations

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,73 @@
-use std::env::args;
+use std::{
+    env::args,
+    io::{BufWriter, Cursor},
+};
 
-use ollama_rs::{Ollama, generation::completion::request::GenerationRequest};
+use base64::{Engine, prelude::BASE64_STANDARD};
+use langchain_rust::{
+    chain::{Chain, LLMChainBuilder},
+    fmt_message, fmt_template,
+    llm::ollama::client::Ollama,
+    message_formatter,
+    prompt::HumanMessagePromptTemplate,
+    prompt_args,
+    schemas::Message,
+    template_fstring,
+};
+use pdf2image::{PDF, RenderOptionsBuilder, image::ImageFormat};
 
 #[async_std::main]
 async fn main() {
     let path = args().nth(1).expect("no file given");
-    let bytes = std::fs::read(path).unwrap();
-    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
-    println!("PDF content: {}", out);
+    println!("PDF filepath: {}", path);
 
-    let ollama = Ollama::default();
-    let model = "deepseek-r1:7b".to_string();
-    let prompt = "give me the name and the location of the event, and the duration for which the ticket is valid, keep in minde that the dates are in european format, please write each information on its own line, get the information from this text: ".to_string()
-        + out.as_str();
+    let pdf = PDF::from_file(&path).unwrap();
+    let pages = pdf
+        .render(
+            pdf2image::Pages::Range(1..=8),
+            RenderOptionsBuilder::default().build().unwrap(),
+        )
+        .unwrap();
 
-    let res = ollama.generate(GenerationRequest::new(model, prompt)).await;
+    let mut writer = BufWriter::new(Cursor::new(Vec::new()));
+    pages[0].write_to(&mut writer, ImageFormat::Jpeg).unwrap();
 
-    if let Ok(res) = res {
-        println!("LLM response : {}", res.response);
+    let buf = writer.into_inner().unwrap().into_inner();
+    let image_b64 = BASE64_STANDARD.encode(&buf);
+
+    let prompt = message_formatter![
+        fmt_message!(Message::new_system_message(
+            "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions."
+        )),
+        fmt_message!(Message::new_human_message_with_images(vec![image_b64])),
+        fmt_template!(HumanMessagePromptTemplate::new(template_fstring!(
+            "{input}", "input"
+        ))),
+    ];
+
+    // let ollama = Ollama::default().with_model("llama3.2-vision");
+    let ollama = Ollama::default().with_model("granite3.2-vision:2b-q8_0");
+    let chain = LLMChainBuilder::new()
+        .prompt(prompt)
+        .llm(ollama)
+        .build()
+        .unwrap();
+
+    match chain
+        .invoke(prompt_args! { "input" =>
+        "Based on this image please find the following informations and give them following precisely this format:
+        Event name:
+        Location of the event:
+        Date and time: (format: HH:DD:MM:YYYY)
+        (if the event takes place over multiple days:)
+        Days during which the ticket is valid:"
+        // "Describe this image"
+         })
+        .await
+    {
+        Ok(result) => {
+            println!("Result: {}", result);
+        }
+        Err(e) => panic!("Error invoking LLMChain: {:?}", e),
     }
 }
